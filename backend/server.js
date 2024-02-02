e50f212fa9dad728febae495cd8640769d3e51c1
Yash Hingane <hingane_y@es.iitr.ac.in>
2024-02-02 13:04:37 +0530

    real time messaging implemented

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,38 @@ app.use("/api/message" , messageRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen( PORT , () => {
+const server = app.listen( PORT , () => {
     console.log(`Listening on port ${PORT}`);
 })
+
+const io = require("socket.io")(server, {
+    pingTimeout: 60000,
+    cors: {
+      origin: "http://localhost:3000",
+    },
+  });
+
+io.on("connection", (socket) => {
+    console.log("Connected to socket.io");
+    socket.on("setup", (userData) => {
+        socket.join(userData?._id);
+        socket.emit("connected");
+    });
+
+    socket.on("joinChat" , (room) => {
+        socket.join(room);
+        console.log("User Joined Room" , room);
+    });
+
+    socket.on("newMessage", (newMessageReceived) => {
+        var chat = newMessageReceived.chat;
+    
+        if (!chat?.users) return console.log("chat.users not defined");
+    
+        chat.users.forEach((user) => {
+          if (user?._id === newMessageReceived?.sender?._id) return;
+        //   console.log(newMessageReceived);
+          socket.in(user._id).emit("messageReceived", newMessageReceived);
+        });
+      });
+});
